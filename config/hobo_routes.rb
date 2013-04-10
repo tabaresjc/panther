c532608e03b3129cc80e5b10577e43db3d44ee7f
Juan Tabares <juan.ctt@live.com>
2013-04-10 17:48:59 +0900

    initial commit

---
+++ config/hobo_routes.rb
@@ -0,0 +1,37 @@
+# This is an auto-generated file: don't edit!
+# You can add your own routes in the config/routes.rb file
+# which will override the routes in this file.
+
+Panther::Application.routes.draw do
+
+
+  # Resource routes for controller users
+  resources :users, :only => [:edit, :show, :create, :update, :destroy] do
+    member do
+      get 'account'
+      put 'accept_invitation', :action => 'do_accept_invitation'
+      get 'accept_invitation'
+      put 'reset_password', :action => 'do_reset_password'
+      get 'reset_password'
+    end
+  end
+
+  # User routes for controller users
+  match 'login(.:format)' => 'users#login', :as => 'user_login'
+  get 'logout(.:format)' => 'users#logout', :as => 'user_logout'
+  match 'forgot_password(.:format)' => 'users#forgot_password', :as => 'user_forgot_password'
+
+  namespace :admin do
+
+
+    # Resource routes for controller admin/users
+    resources :users do
+      collection do
+        post 'invite', :action => 'do_invite'
+        get 'invite'
+      end
+    end
+
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Panther::Application.config.secret_token = '97563e83502e88a1f87642bea071bf4c6b6cd5bf3c2fee258ac08df8715c98dfa483753a99c2a31e4b881dfd89e4cb8f81cd244ef42763ce3479282531e6689d'
